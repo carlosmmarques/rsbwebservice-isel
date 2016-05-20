40b766d25446d5ae0585c8c4ca0e68a0bed6e5cf
Venturinha <tventurinha@gmail.com>
2016-05-20 15:49:13 +0100

    Estrutura inicial do RSB-WebClient.

---
+++ RSB-WebClient/src/main/java/pt/isel/ps/li61n/RsbWebClientApplication.java
@@ -0,0 +1,12 @@
+package pt.isel.ps.li61n;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsbWebClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RsbWebClientApplication.class, args);
+	}
+}
---
+++ RSB-WebClient/src/test/java/pt/isel/ps/li61n/RsbWebClientApplicationTests.java
@@ -0,0 +1,18 @@
+package pt.isel.ps.li61n;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = RsbWebClientApplication.class)
+@WebAppConfiguration
+public class RsbWebClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
